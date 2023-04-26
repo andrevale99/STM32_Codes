82453c6c070ea27dc2ad620631249a3c7dab2a5f
andrevale99 <andrefvale@gmail.com>
2023-04-26 17:54:10 -0300

    Codigo finalizado

--- CMSIS_examples/TIMER_example/src/main.c
+++ CMSIS_examples/TIMER_example/src/main.c
@@ -5,24 +5,28 @@
 //============================================================
 //  VARIVEIS
 //============================================================
-
+volatile uint8_t cnt = 0;
 //============================================================
 //  PROTOTIPOS
 //============================================================
 void GPIO_Setup(void);
-void TIMER3_Setup(void);
+void TIMER4_Setup(void);
 
-void TIMER3_IRQHandler(void);
+void TIM3_IRQHandler(void);
 //============================================================
 //  MAIN
 //============================================================
 int main(void)
 {
     GPIO_Setup();
-    TIMER3_Setup();
+    TIMER4_Setup();
     
     while(1)
     {
+        /*if(TIM3->CNT > 5000)
+            GPIOC->BSRR |= GPIO_BSRR_BR13;
+        else
+            GPIOC->BSRR |= GPIO_BSRR_BS13;*/
     }
 
     return 0;
@@ -39,33 +43,38 @@ void GPIO_Setup(void)
     GPIOC->OSPEEDR = 0;
     GPIOC->PUPDR = 0;
 
-    GPIOC->ODR |= GPIO_ODR_OD13;
+    GPIOC->BSRR |= GPIO_BSRR_BR13;
 }
 
-void TIMER3_Setup(void)
+void TIMER4_Setup(void)
 {
     //TIMER3 de 16 bits ativado
     RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
 
-    //Carrega o valor maximo
-    TIM3->ARR = 15635;
+    //Carrega o valor maximo da contagem
+    TIM3->ARR = (uint16_t)15635/10;
 
+    //Ativa o prescale do contador
     TIM3->PSC = 1025;
 
     //Ativa o evento
     TIM3->EGR |= TIM_EGR_UG;
 
+    //Ativa a interrupcao
     TIM3->DIER |= TIM_DIER_UIE;
 
-    //Ativa a contagem
+    //Ativa a interrupcao somente pelo overflow/underflow ou DMA
+    //e ativa o contador
     TIM3->CR1 |= TIM_CR1_URS | TIM_CR1_CEN;
 
     NVIC_EnableIRQ(TIM3_IRQn);
     NVIC_SetPriority(TIM3_IRQn, 0);
 }
 
-void TIMER3_IRQHandler(void)
+void TIM3_IRQHandler(void)
 {
-    TIM3->CR1 |= TIM_CR1_CEN;
+    //Sempre desativar antes de fazer qualquer coisa
+	TIM3->SR &= ~TIM_SR_UIF;
+
     GPIOC->ODR ^= GPIO_ODR_OD13;
 }
