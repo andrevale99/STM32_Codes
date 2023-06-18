a2cd214d1673531c9dfa6aa82c3b0f70c4261375
andrevale99 <andrefvale@gmail.com>
2023-06-17 23:07:14 -0300

    Ativando o TX do STM e RX do Atmega

--- CMSIS_examples/USART_example/src/main.c
+++ CMSIS_examples/USART_example/src/main.c
@@ -3,7 +3,7 @@
 //============================================================
 //  VARIVEIS
 //============================================================
-volatile uint32_t data = 0;
+volatile uint8_t data = 24;
 //============================================================
 //  PROTOTIPOS
 //============================================================
@@ -19,6 +19,7 @@ int main(void)
 
     while(1)
     {
+
     }
 
     return 0;
@@ -30,9 +31,9 @@ void USART_setup()
 {
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
 
-    //PINO A3 como RX
-    GPIOA->MODER |= GPIO_MODER_MODER3_1;
-    GPIOA->AFR[0] |= (7<<GPIO_AFRL_AFSEL3_Pos);
+    //PINO A2 como TX
+    GPIOA->MODER |= GPIO_MODER_MODER2_1;
+    GPIOA->AFR[0] |= (7<<GPIO_AFRL_AFSEL2_Pos);
     
     RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
 
@@ -42,8 +43,8 @@ void USART_setup()
     USART2->BRR = ( ( ( uartdiv / 16 ) << USART_BRR_DIV_Mantissa_Pos ) |
     				( ( uartdiv % 16 ) << USART_BRR_DIV_Fraction_Pos ) );
 
-    //Ativa a interrupcao e somente o RX do MCU
-    USART2->CR1 |= USART_CR1_RE | USART_CR1_RXNEIE | USART_CR1_UE;
+    //Ativa a interrupcao e somente o TX do MCU
+    USART2->CR1 |= USART_CR1_TE | USART_CR1_TCIE | USART_CR1_UE;
 
     NVIC_EnableIRQ(USART2_IRQn);
     NVIC_SetPriority(USART2_IRQn, 0);
@@ -51,8 +52,11 @@ void USART_setup()
 
 void USART2_IRQHandler(void)
 {
-    //Bit de interrupcao limpo quando o registrador
-    //de dado é lido
-    if((USART2->SR & USART_SR_RXNE))
-        data = USART2->DR;
+    //Bit de interrupcao limpo quando a
+    //transmissao eh completada
+    if((USART2->SR & USART_SR_TC))
+    {
+        USART2->SR &= ~USART_SR_TC;
+        USART2->DR = data;
+    }
 }
--- CMSIS_examples/USART_example/test/atmega328p_USART.c
+++ CMSIS_examples/USART_example/test/atmega328p_USART.c
@@ -30,25 +30,21 @@
 #define ToggleBit(port, pin) (port ^= (1<<pin))
 #define TestBit(port, pin) (port & (1<<pin))
 
-#define BAUD 9600
+#define BAUD 115200
 #define MYUBRR F_CPU/16/BAUD-1
 
 //===================================================
 //  VARIAVEIS
 //===================================================
-volatile uint8_t idx = 0x00; //indice para percorrer o vetor
-const char msg[] = "TX COMPLETE\n"; //vetor para transmitir na serial
-volatile bool tx_em_acao = false; //booleano para indicar fim da operacao de trnasmissao
+volatile uint8_t rx_data = 0;
 //===================================================
 //  PROTOTIPOS
 //===================================================
 void setup();
 
 void USART_Init(unsigned int ubrr);
-void USART_Transmit(unsigned char data);
 
-ISR(USART_UDRE_vect);
-ISR(USART_TX_vect);
+ISR(USART_RX_vect);
 //===================================================
 //  MAIN
 //===================================================
@@ -59,11 +55,8 @@ int main()
 
     for(;;)
     {
-        //Caso pressione o botao, comece a transmissao
-        //Ativa o bit de interrupcao de Verificar se o registrador
-        //UDR0 esta vazio
-        if(!(PINB & (1<<PINB0)) && !tx_em_acao)
-            SetBit(UCSR0B, UDRIE0);
+        if(rx_data % 2 == 0 && rx_data != 0)
+            ToggleBit(PORTB, PB5);
     }
 
     return 0;
@@ -79,8 +72,7 @@ void setup()
 {
     SetBit(DDRB, DDB5);
     
-    ClrBit(DDRB, PB0);
-    SetBit(PORTB, PB0);
+    ClrBit(PORTB, PB5);
 
     USART_Init(MYUBRR);
 }
@@ -101,8 +93,11 @@ void USART_Init(unsigned int ubrr)
 
     /*Enable transmitter 
     Desativa a interrupcao Data Empty
-    Desativa a interrupcao complete*/
-    SetBit(UCSR0B, TXEN0);
+    Desativa a interrupcao TX complete*/
+    SetBit(UCSR0B, RXEN0);
+    SetBit(UCSR0B, RXCIE0);
+
+    ClrBit(UCSR0B, TXEN0);
     ClrBit(UCSR0B, TXCIE0);
     ClrBit(UCSR0B, UDRIE0);
 
@@ -112,71 +107,12 @@ void USART_Init(unsigned int ubrr)
 }
 
 /**
- * @brief Transmissao de um byte pelo USART0
- * @param data caractere
- * 
- * @note Essa funcao é utilizada quando nao for utilizada
- * as interrupcoes para envio de dado
-*/
-void USART_Transmit(unsigned char data)
-{
-    /* Wait for empty transmit buffer */
-    while (!(UCSR0A & (1<<UDRE0))) 
-        ;
-    /* Put data into buffer, sends the data */
-    UDR0 = data;
-}
-
-/**
- * @brief USART, data register empty
- * 
- * @note Entra nesta interrupcao quando
- * nao haver dado no registrador UDR0
- * 
- * @note Aqui ele comeca a operacao de transmissao
- * envia um dado para sinalizar algo, para depois 
- * desativar esta interrupcao e ativa a interrupcao de TX
-*/
-ISR(USART_UDRE_vect)
-{
-    if(!(PINB & (1<<PINB0)))
-    {
-        do
-        {
-            SetBit(PORTB, PB5);
-        } while (!(PINB & (1<<PINB0)));
-        ClrBit(PORTB, PB5);
-
-        tx_em_acao = true;
-        UDR0 = '&';
-        idx = 0;
-    }
-
-    ClrBit(UCSR0B, UDRIE0);
-    SetBit(UCSR0B, TXCIE0);
-}
-
-/**
- * @brief USART, Tx complete
+ * @brief USART, Rx complete
  * 
  * @note Entra nesta interrupcao quando a 
- * transmissao for completa
- * 
- * @note Ao comecar a transmissao, percorre o vetor a
- * cada fim de transmissao, caso termine de percorrer o vetor
- * troca a flag booleana e sai da interrupcao antes
- * de enviar o '\0'
+ * Recepcao for completa
 */
-ISR(USART_TX_vect)
+ISR(USART_RX_vect)
 {
-    if(msg[idx]=='\0')
-    {
-        ClrBit(UCSR0B, TXCIE0);
-        idx = 0;
-        tx_em_acao = false;
-        return;
-    }
-        
-    UDR0 = msg[idx];
-    ++idx;
+    rx_data = UDR0;
 }
