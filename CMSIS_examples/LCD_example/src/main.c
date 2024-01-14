79a9f011b84a18d0ff263c0cb8ecbfc35077e7f1
andrevale99 <andrefvale@gmail.com>
2024-01-13 21:46:18 -0300

    Inclusão da biblioteca do Systick Timer e da função de interrupção

--- CMSIS_examples/LCD_example/src/main.c
+++ CMSIS_examples/LCD_example/src/main.c
@@ -1,6 +1,12 @@
 #include <stm32f411xe.h>
+#include "Systick_Timer.h"
 
 #include <LCD_stm32.h>
+
+void SysTick_Handler(void)
+{                               /* SysTick interrupt Handler. */
+  msTicks++;                                        
+}
 
 int main(void)
 {
