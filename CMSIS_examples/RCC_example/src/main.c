952f66d4d8f3785386c795e568b9adc9e9711057
andrevale99 <andrefvale@gmail.com>
2026-01-18 16:53:36 -0300

    Exemplo Refeito

--- CMSIS_examples/RCC_example/src/main.c
+++ CMSIS_examples/RCC_example/src/main.c
@@ -2,24 +2,18 @@
 
 int main(void)
 {
+    //Ativa e espera o HSE ficar pronto
     RCC->CR |= RCC_CR_HSEON;
     while (!(RCC->CR & RCC_CR_HSERDY))
         ;
 
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
-    __DSB();
+    // Coloca o HSE como clock do sistema
+    RCC->CFGR |= RCC_CFGR_SW_HSE;
+    while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_HSE)
+        ;
 
-    GPIOC->MODER |= GPIO_MODER_MODE13_0;
-    GPIOC->BSRR |= GPIO_BSRR_BS13;
-    GPIOC->OTYPER = 0;
-    GPIOC->OSPEEDR = 0;
-    GPIOC->PUPDR = 0;
-
-    // No black PILL o led PC13 ativa no 0
-    if ((RCC->CR & RCC_CR_HSERDY))
-        GPIOC->ODR &= ~GPIO_ODR_OD13;
-    else
-        GPIOC->ODR |= GPIO_ODR_OD13;
+    // Desativa o HSI para economizar energia
+    RCC->CR &= ~RCC_CR_HSION;
 
     while (1)
     {
