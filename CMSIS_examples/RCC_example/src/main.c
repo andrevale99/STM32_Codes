4dd7d1ec57db645f1ee2172ac347b168e122c95a
andrevale99 <andrefvale@gmail.com>
2026-01-28 10:18:07 -0300

    Exemplo de como mudar o clock e verificar no PA8

--- CMSIS_examples/RCC_example/src/main.c
+++ CMSIS_examples/RCC_example/src/main.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-    //Ativa e espera o HSE ficar pronto
+    // Ativa e espera o HSE ficar pronto
     RCC->CR |= RCC_CR_HSEON;
     while (!(RCC->CR & RCC_CR_HSERDY))
         ;
@@ -13,7 +13,20 @@ int main(void)
         ;
 
     // Desativa o HSI para economizar energia
-    RCC->CR &= ~RCC_CR_HSION;
+    // RCC->CR &= ~RCC_CR_HSION;
+
+    RCC->CFGR &= ~(RCC_CFGR_MCO1 | RCC_CFGR_MCO1PRE);
+    // RCC->CFGR |= (0x0 << 21);     // HSI
+    RCC->CFGR |= RCC_CFGR_MCO1_1; // HSE
+
+    // Habilita clock do GPIOA
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+
+    // PA8 em Alternate Function
+    GPIOA->MODER |= (2 << 16);
+
+    // AF0 para PA8 (MCO1)
+    GPIOA->AFR[1] &= ~(0xF << 0);
 
     while (1)
     {
